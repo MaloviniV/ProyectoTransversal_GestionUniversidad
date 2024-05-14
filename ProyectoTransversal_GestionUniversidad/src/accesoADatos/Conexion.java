13ba38302c160351719a3e86e17985f5084b0b3c
Victor Malovini <malovinivjd@hotmail.com>
2024-05-14 16:31:21 -0300

    Carga de proyecto

---
+++ ProyectoTransversal_GestionUniversidad/src/accesoADatos/Conexion.java
@@ -0,0 +1,9 @@
+package accesoADatos;
+
+public class Conexion {
+    private static final String URL = "jdbc:mysql://localhost/";
+    private static final String DB = "universidad_ulp";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD;
+    private static final String;
+}
