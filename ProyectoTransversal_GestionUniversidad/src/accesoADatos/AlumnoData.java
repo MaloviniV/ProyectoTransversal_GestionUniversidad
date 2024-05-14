aa938fe52b85d408e54d66dc1f1b4c7203a91265
AlvaroAdamek <adameklisandro@gmail.com>
2024-05-14 17:31:00 -0300

    s1

--- ProyectoTransversal_GestionUniversidad/src/accesoADatos/AlumnoData.java
+++ ProyectoTransversal_GestionUniversidad/src/accesoADatos/AlumnoData.java
@@ -5,7 +5,16 @@
  */
 package accesoADatos;
 
+import entidades.Alumno;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,6 +25,24 @@ public class AlumnoData {
     
     public AlumnoData(){
         con = Conexion.getConexion();
+    }
+    
+    public void guardarAlumno(Alumno alumno){
+        String SQL = "INSERT INTO alumno (dni, apellido, nombre,"
+                + " fechaNacimiento, estado) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
+            ps.setBoolean(5, alumno.isActivo());
+            ps.executeUpdate();
+            
+            ResultSet re = ps.getGeneratedKeys();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno");
+        }
     }
     
 }
